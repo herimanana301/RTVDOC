8bd5c58f80852cec62493ee1b610fd80785963a2
nytoavinaras <rasamimanana.nytoavina@gmail.com>
2024-01-18 20:35:08 +0400

    Facture.jsx

--- src/components/facture/Facture.jsx
+++ src/components/facture/Facture.jsx
@@ -1,52 +1,118 @@
 import { useState } from "react";
-import { Autocomplete, Button, Menu, NativeSelect } from "@mantine/core";
+import { ActionIcon, Anchor, Autocomplete, Button, Group, Menu, Modal, NativeSelect, ScrollArea, Table, Text  } from "@mantine/core";
 import { IconSearch, IconFilter } from "@tabler/icons-react";
+import { IconBolt } from "@tabler/icons-react";
 
 export default function Facture() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleModal = () => {
+    setIsModalOpen(true);
+  };
+
   const [menuVisible, setMenuVisible] = useState(false);
   const handleMenuToggle = () => {
     setMenuVisible(!menuVisible);
   };
+  const tableData = [
+    { id: 1, name: "Facture N° 8395/24", label: "Herimanana Rasolonirina", period: "Du 2024-01-16 au 2024-01-31", qte: "70", pu: "10000"},
+    { id: 2, name: "Facture N° 8396/24", label: "Telma", period: "Du 2024-11-27 au 2024-12-27", qte: "70", pu: "10000"},
+  ];
+  const rows = tableData.map((item) => (
+    <tr key={item.id}>
+      <td>{item.name}</td>
+      <td>{item.label}</td>
+      <td>{item.period}</td>
+      <td>{item.qte}</td>
+      <td>{item.pu}</td>
+      <td>{item.qte * item.pu}</td>
+      <td>
+        <Group spacing={0} position="right">
+          <ActionIcon onClick={ handleModal }>
+            <IconBolt size="1rem" stroke={1.5}/>
+          </ActionIcon>
+        </Group>
+      </td>
+    </tr>
+  ));
   return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Button component="a" href="/newinvoice">
-        + Nouveau
-      </Button>
-      <Autocomplete
-        placeholder="Rechercher"
-        icon={<IconSearch size="1rem" stroke={1.5} />}
-        data={[]}
-      />
-      <Menu
-        shadow="md"
-        width={"auto"}
-        position="left"
-        offset={5}
-        opened={menuVisible}
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Button component="a" href="/newinvoice">
+          + Nouveau
+        </Button>
+        <Autocomplete
+          placeholder="Rechercher"
+          icon={<IconSearch size="1rem" stroke={1.5} />}
+          data={[]}
+        />
+        <Menu
+          shadow="md"
+          width={"auto"}
+          position="left"
+          offset={5}
+          opened={menuVisible}
+        >
+          <Menu.Target>
+            <Button onClick={handleMenuToggle}>
+              <IconFilter size="1.1rem" stroke={2} />
+              Filtre
+            </Button>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item>
+              <NativeSelect
+                data={["", "En attente de diffusion", "En cours de diffusion", "Diffusion terminée"]}
+                label="État de diffusion"
+                radius="md"
+              />
+            </Menu.Item>
+            <Menu.Item>
+              <NativeSelect
+                data={["", "Payé", "Non Payé"]}
+                label="Ètat de paiement"
+                radius="md"
+              />
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </div>
+      {/* Table */}
+      <ScrollArea>
+      <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
+        <thead>
+          <tr>
+            <th> Nom </th>
+            {/*<th>Récapitulation</th>*/}
+            <th>Libellé</th>
+            <th>Période</th>
+            <th>Qte</th>
+            <th>P.U.</th>
+            <th>Montant</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </ScrollArea>
+    <Modal
+      opened={isModalOpen}
+      onClose={() => setIsModalOpen(false)}
+      style={{ alignItems: 'center'}}
+      title={<Text fz="sm" fw={500}>Aperçu de la facture</Text>}
+      centered
+    >
+      
+      
+      <Button
+        variant="gradient"
+        gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+        style = {{ marginTop: 15}}
       >
-        <Menu.Target>
-          <Button onClick={handleMenuToggle}>
-            <IconFilter size="1.1rem" stroke={2} />
-            Filtre
-          </Button>
-        </Menu.Target>
-        <Menu.Dropdown>
-          <Menu.Item>
-            <NativeSelect
-              data={["", "En attente de diffusion", "En cours de diffusion"]}
-              label="État de diffusion"
-              radius="md"
-            />
-          </Menu.Item>
-          <Menu.Item>
-            <NativeSelect
-              data={["", "Payé", "Non Payé"]}
-              label="Ètat de paiement"
-              radius="md"
-            />
-          </Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
+        Imprimer
+      </Button>
+    </Modal>
     </div>
+    
+    
   );
 }
