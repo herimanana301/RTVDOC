f04f1658382d847b96f8675c7b484c7f664a291d
Herimanana Rasolonirina <herimanana301@gmail.com>
2024-01-25 20:21:46 +0300

    hmm

--- src/components/general/General.jsx
+++ src/components/general/General.jsx
@@ -1,4 +1,4 @@
-import { Grid, Skeleton, Container, Paper, Text, Flex } from "@mantine/core";
+import { Grid, Skeleton, Container, Paper, Text } from "@mantine/core";
 import { useEffect, useState } from "react";
 import urls from "../../services/urls";
 import axios from "axios";
@@ -9,7 +9,8 @@ export default function General() {
     orderNumber: 0,
     personelNumber: 0,
   });
-
+  const [invoice, setInvoice] = useState([]);
+  const [totalTurnOver, setTotalTurnOver] = useState(0);
   useEffect(() => {
     axios.get(`${urls.StrapiUrl}api/clients?_limit=-1`).then((response) => {
       setDataNumber((prevData) => {
@@ -21,19 +22,20 @@ export default function General() {
         return { ...prevData, orderNumber: response.data.data.length };
       });
     });
-    axios.get(`${urls.StrapiUrl}api/personnel?_limit=-1`).then((response) => {
+    axios.get(`${urls.StrapiUrl}api/personnels?_limit=-1`).then((response) => {
       setDataNumber((prevData) => {
         return { ...prevData, personelNumber: response.data.data.length };
       });
     });
-    axios.get(`${urls.StrapiUrl}api/commandes?_limit=-1`).then((response) => {
-      setDataNumber((prevData) => {
-        return { ...prevData, orderNumber: response.data.data.length };
-      });
+    axios.get(`${urls.StrapiUrl}api/payements?_limit=-1`).then((response) => {
+      setInvoice(response.data.data);
     });
   }, []);
-
-  const child = <Skeleton height={140} radius="md" animate={false} />;
+  useEffect(() => {
+    if (invoice.length > 0) {
+      console.log(invoice);
+    }
+  }, [invoice]);
 
   return (
     <Container my="md">
@@ -71,7 +73,7 @@ export default function General() {
             <Text>Nombre de commande</Text>
           </Paper>
         </Grid.Col>
-        <Grid.Col xs={8}>
+        <Grid.Col xs={5}>
           <Paper
             shadow="xs"
             radius="xl"
@@ -87,6 +89,22 @@ export default function General() {
               {dataNumber.personelNumber}
             </Text>
             <Text>Nombre de personnel</Text>
+          </Paper>
+        </Grid.Col>
+        <Grid.Col xs={13}>
+          <Paper
+            shadow="xs"
+            radius="xl"
+            p="xl"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>Chiffre d'affaire</Text>
+            <Text style={{ fontSize: "4rem" }}></Text>
           </Paper>
         </Grid.Col>
       </Grid>
--- src/components/input/NewClient.jsx
+++ src/components/input/NewClient.jsx
@@ -131,7 +131,6 @@ export default function NewClient() {
       });
   };
   useEffect(() => {
-    console.log(currentClient);
     if (id) {
       updateDescription(0, currentClient.raisonsocial);
       updateDescription(1, currentClient.email);
