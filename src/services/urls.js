cb692604d894f52c3e8d65244e840b5183f6ddbc
Stenn <stennjs09@gmail.com>
2024-02-29 11:37:58 +0300

    update on facture fonctionnalities relying directly with command form commad->facture print..

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,6 +1,6 @@
 const urls = {
-    // StrapiUrl : "http://192.168.0.39:1337/",
-    StrapiUrl : "http://localhost:1337/",
+    StrapiUrl : "http://192.168.0.39:1337/",
+    // StrapiUrl : "http://localhost:1337/",
     ForUpload : "http://192.168.0.39:1337",
 }
 
